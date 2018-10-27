3ecc56e77b1df836e86e6d0488cd42d3e6ec50aa
Isankats <isankats@gmail.com>
2018-10-27 13:25:17 -0700

    New Line

--- Java/ForLoop.java
+++ Java/ForLoop.java
@@ -3,7 +3,7 @@ class whileLoopDemo
     public static void main(String args[]) 
     { 
         int x = 1; 
-        while (x <= 10) 
+        while (x <= 20) 
         { 
             System.out.println("Value of x:" + x); 
             x++; 
